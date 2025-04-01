a1316e2951cd5308f1c6a767aaa51f9264c4c32a
maxim <maxpawgdbs@yandex.ru>
2025-04-01 11:26:05 +0300

    qwretwryetjryhtheryterwetwryetuytktyelrjewtqARPYDSIAEWRI[0QEORJPIADHOGPO0S[FAETUPqrkjxcxpoisatrgfm cxbnklgoiaskjtrgobniopxgf0dtakjlmn miopdgewriocxgf0-ogzkjmi90imrl

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import http.server
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        return b"Hello world"
+        self.wfile.write(open("main.html", "rb").read())
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        self.wfile.write(post_data)
 
 
-server = http.server.HTTPServer(("localhost", 8000), http.server.SimpleHTTPRequestHandler)
+server = http.server.HTTPServer(("localhost", 8000), Handler)
 server.serve_forever()
