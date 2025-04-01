e612e4fade056fd2812d8ccda9440a9b418796bd
maxim <maxpawgdbs@yandex.ru>
2025-04-01 11:45:53 +0300

    sosaslaslaslldasaflgado;gadwkl[

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import http.server
 
 class OurHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.wfile.write(open("main.html", "rb").read())
+        if self.path == "/" or self.path == "":
+            self.wfile.write(open("main.html", "rb").read())
+        else:
+            try:
+                self.wfile.write(open(self.path[1:], "rb").read())
+            except Exception as e:
+                print("пошёл нахуй", e)
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
